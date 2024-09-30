22b291b502e3d485163a8b2e0690dda6ca1315d9
Wendelsena <wendel4019@gmail.com>
2024-09-30 16:51:41 -0300

    Alteração no pubdpec e tela home

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,84 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp()); 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Criação de um widget chamado `MyApp` que estende `StatelessWidget`.
+class MyApp extends StatelessWidget { // `StatelessWidget` é um tipo de widget que não possui estado mutável, ou seja, ele é ideal para construir partes da interface que não precisam ser atualizadas dinamicamente após a criação.
+  const MyApp({super.key}); // O `super.key` é usado para passar a chave ao widget pai (`StatelessWidget`). Isso ajuda na identificação de widgets e em processos como reconstrução eficiente.
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  @override // Indica que o método abaixo está substituindo um método da classe pai (`StatelessWidget`).
+  Widget build(BuildContext context) { // Método `build()` é responsável por construir a interface do usuário e recebe `BuildContext context` que contém informações sobre o local do widget na árvore de widgets.
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp( // `MaterialApp` é a base do aplicativo que fornece estrutura para usar componentes de Material Design.
+      home: Scaffold(  // `Scaffold` fornece a estrutura básica de uma página, como barra de app, corpo, rodapé, etc.
+        body: Container( // `Container` é um widget versátil que pode conter outros widgets e permite customização como margem, padding, alinhamento, etc.
+          decoration: const BoxDecoration(  // `BoxDecoration` é usado para decorar o `Container`, adicionando um plano de fundo, borda, etc.
+            image: DecorationImage(  // `DecorationImage` define uma imagem como decoração do `Container`.
+              image: AssetImage('assets/wallpaper.jpg'),  // `AssetImage` é usado para carregar uma imagem dos recursos do aplicativo.
+              fit: BoxFit.cover, // para fazer a imagem ocupar a tela toda.
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric( // Define o preenchimento interno
+                    vertical: 20, // Aumenta a margem interna vertical
+                    horizontal: 30, // Aumenta a margem interna horizontal
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(248, 255, 255, 244), // Cor de fundo do Container
+                    borderRadius: BorderRadius.circular(10), // Arredondamento das bordas
+                    boxShadow: const [ // Adiciona sombra
+                      BoxShadow(
+                        color: Colors.black38, // Cor da sombra
+                        offset: Offset(0, 4), // Posição da sombra
+                        blurRadius: 10, // Difusão da sombra
+                        spreadRadius: 1, // Espalhamento da sombra
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    "COCOLATOR",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 57, 29, 9),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 57, 29, 9),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    side: const BorderSide( // borda branca no botão
+                      color: Colors.white,
+                      width: 2,
+                    )
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 80,
+                    ),
+                  ),
+                  child: const Text(
+                    "Iniciar",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20),
+                  ),
+                )
+              ],
+            )  // `Center` alinha seu filho no centro do widget pai.
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
